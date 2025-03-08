97b1c4f674b664d110116deca196467ef9096fdb
Tore Eide Andersen <tore.eide.andersen@kantega.no>
2025-03-08 15:33:12 +0100

    chore: comment

--- src/main/java/io/github/torand/fastersql/relation/Relation.java
+++ src/main/java/io/github/torand/fastersql/relation/Relation.java
@@ -3,6 +3,9 @@ package io.github.torand.fastersql.relation;
 import io.github.torand.fastersql.Sql;
 import io.github.torand.fastersql.alias.TableAlias;
 
+/**
+ * Represents a table of columns and rows, whether a permanent database table or a temporary table (the result of a subquery).
+ */
 public interface Relation extends Sql {
 
     Relation as(String alias);
